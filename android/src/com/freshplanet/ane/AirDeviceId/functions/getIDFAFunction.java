71647b5ab044b6ccf11bf518c9798f693e870bec
Adam Schlesinger <adam@freshplanet.com>
2014-08-01 16:52:38 -0400

    event needs to be dispatched on the ui thread

--- android/src/com/freshplanet/ane/AirDeviceId/functions/getIDFAFunction.java
+++ android/src/com/freshplanet/ane/AirDeviceId/functions/getIDFAFunction.java
@@ -9,10 +9,8 @@ import android.content.Context;
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
-import com.adobe.fre.FREWrongThreadException;
 import com.freshplanet.ane.AirDeviceId.AirDeviceIdExtension;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-import com.google.android.gms.ads.identifier.AdvertisingIdClient.Info;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 
@@ -24,20 +22,15 @@ public class getIDFAFunction implements FREFunction {
 
 		final Activity	activity	= arg0.getActivity();
 		final Context	context		= activity.getApplicationContext();
-		
+
 		// getAdvertisingIdInfo needs to be handled in a separate thread
 		Thread idfaThread = new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				
-				Info	adInfo	= null;
-				String	adId	= null;
-				
 				try {
-					
-					adInfo 	= AdvertisingIdClient.getAdvertisingIdInfo(context);
-					adId	= adInfo.getId();
+					finished(activity, AdvertisingIdClient.getAdvertisingIdInfo(context));
 				} 
 				catch (IllegalStateException e) {
 					// TODO Auto-generated catch block
@@ -55,13 +48,27 @@ public class getIDFAFunction implements FREFunction {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				AirDeviceIdExtension.context.dispatchStatusEventAsync(IDFA_VALUE, adId);
 			}
 		});
 		
 		idfaThread.start();
 		
 		return null;
+	}
+	
+	private void finished(final Activity activity, final AdvertisingIdClient.Info adInfo){
+		
+	    if (adInfo != null) {
+
+	    	activity.runOnUiThread(new Runnable() {
+	        	
+	            @Override
+	            public void run() {
+
+	            	String idfa = adInfo.getId();
+	            	AirDeviceIdExtension.context.dispatchStatusEventAsync(IDFA_VALUE, idfa);
+	            }
+	        });
+	    }
 	}
 }
